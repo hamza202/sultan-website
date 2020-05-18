8de4feea1267c806c41f95e5c9220b2f6fc38e2c
unknown <k8668975@gmail.com>
2020-05-19 02:16:20 +0300

    filter page and remove some file not necessary and add fontawesome library

--- js/main.js
+++ js/main.js
@@ -47,3 +47,22 @@ if ($('.my-owl-carousel2').length){
         }
     })
 }
+let price_input = $('#price_input');
+if (price_input.length) {
+    price_input.ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 800,
+        prefix: "LYD ",
+    });
+}
+price_input.on("change", function () {
+    var $inp = $(this);
+    var from = $inp.prop("value"); // reading input value
+    var from2 = $inp.data("from"); // reading input data-from attribute
+
+    console.log(from, from2); // FROM value
+});
